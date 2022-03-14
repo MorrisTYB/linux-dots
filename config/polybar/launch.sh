d96b55d5e125043950689158a030dd5ccf3012e8
Morris Alromhein <morris4028@gmail.com>
2022-03-14 04:47:32 -0400

    Initial Commit

---
+++ config/polybar/launch.sh
@@ -0,0 +1,11 @@
+# killall -q polybar 
+# polybar example 
+#!/bin/sh
+killall polybar
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload example &
+  done
+else
+  polybar --reload example &
+fi
